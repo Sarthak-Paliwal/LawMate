baf77bc784c38a1bec95ef2412d3aac37211f8cb
Sarthak Paliwal <paliwalsarthak03@gmail.com>
2026-04-10 12:50:44 +0530

    Cleaned Folders

--- lawmate-backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js', '**/?(*.)+(spec|test).js'],
-  clearMocks: true,
-  restoreMocks: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.js', '!src/app.js', '!src/server.js'],
-};
